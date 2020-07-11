6e890abca39b195853808fd4ec5b152a8b8d49fe
neijrdev <nei.jr.design@gmail.com>
2020-07-11 03:24:32 -0300

    Create Controllers Next Level Week - BackEnd

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,20 @@
 import express from 'express'
-import knex from './database/conection'
 
 import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express()
 
-const pointsController = new PointsController
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response)=>{
-  const items = await knex('items').select('*');
+routes.get('/items', itemsController.index)
 
-  const serializedItems = items.map(item =>{
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
-    }
-  })
-
-  return response.json(serializedItems)
-})
+routes.get('/points', pointsController.index)
 
 routes.post('/points', pointsController.create)
+
+routes.get('/points/:id', pointsController.show)
 
 export default routes;
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors'
 import routes from './routes';
 import path from 'path';
 
 const app = express();
+
+app.use(cors({
+  origin: 'localhost:3000'
+}))
 
 app.use(express.json());
 
